4e296a2514040c7034f38049154d8195d07c596c
tamilvanan12584 <tamilvanan12584@users.noreply.github.com>
2018-03-21 17:11:54 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	 char name[20];
+	 int a,b,c;
+	 char abc[10];
+    printf("Enter Your Name \n");
+    scanf("%[^\n]s", &name);
+		printf("Enter Value of A: \n");
+		scanf("%d", &a);		printf("Enter Value of B: \n");
+		scanf("%d", &b);
+		c=a+b;
+		printf ("Your Name is %s \n  ", name);
+		printf ("Sum of A and B is: %d \n", c);
+		return 0;
+		}
